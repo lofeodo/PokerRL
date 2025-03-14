7b6e4524181601719828090feffbe64e9c3b359c
lofeodo <daniel.lofeodo@gmail.com>
2025-03-14 09:41:00 -0400

    fix data generation

--- models/PyStack/src/Settings/arguments.py
+++ models/PyStack/src/Settings/arguments.py
@@ -44,10 +44,10 @@ class Parameters():
 		self.data_path = './data/TrainSamples/'
 		# self.data_path = r'D:\Datasets\Pystack\NoLimitTexasHoldem'
 		# the number of iterations that DeepStack runs CFR for
-		self.cfr_iters = 800
+		self.cfr_iters = 100
 		# the number of preliminary CFR iterations which DeepStack doesn't
 		# factor into the average strategy (included in cfr_iters)
-		self.cfr_skip_iters = 500
+		self.cfr_skip_iters = 60
 		# the number of starting iters used on approximating leaf nodes
 		# after these iterations next street's root nodes are approximated and averaged
 		# no need for 'river', because you get values from leaf nodes anyway (using terminal equity)
@@ -57,7 +57,7 @@ class Parameters():
 			'turn':200
 		}
 		# how many solved poker situations are generated
-		self.gen_different_boards = 2
+		self.gen_different_boards = 50
 		# how many poker situations are solved simultaneously during
 		# data generation
 		self.gen_batch_size = 100
