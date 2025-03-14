736a51074b07cfd01838e3365a670572dcf1ff76
lofeodo <daniel.lofeodo@gmail.com>
2025-03-14 14:31:57 -0400

    Add testing arguments

--- models/PyStack/src/Settings/arguments.py
+++ models/PyStack/src/Settings/arguments.py
@@ -31,23 +31,13 @@ class Parameters():
 		# the neural net architecture
 		self.num_neurons = [500,500,500,500] # must be size of num_layers
 		self.learning_rate = 1e-4
-		self.batch_size = 256
-		self.num_epochs = 50
-		# how often to save the model during training
-		self.save_epoch = 2
-		# how many epochs to train for
-		self.epoch_count = 10
 		# TF RECORDS
 		self.tfrecords_batch_size = 1024*10 # ~200MB
 		# DATA GENERATION
 		# path to the solved poker situation data used to train the neural net
 		self.data_path = './data/TrainSamples/'
 		# self.data_path = r'D:\Datasets\Pystack\NoLimitTexasHoldem'
-		# the number of iterations that DeepStack runs CFR for
-		self.cfr_iters = 100
-		# the number of preliminary CFR iterations which DeepStack doesn't
-		# factor into the average strategy (included in cfr_iters)
-		self.cfr_skip_iters = 60
+		
 		# the number of starting iters used on approximating leaf nodes
 		# after these iterations next street's root nodes are approximated and averaged
 		# no need for 'river', because you get values from leaf nodes anyway (using terminal equity)
@@ -56,14 +46,24 @@ class Parameters():
 			'flop':200,
 			'turn':200
 		}
-		# how many solved poker situations are generated
-		self.gen_different_boards = 50
-		# how many poker situations are solved simultaneously during
-		# data generation
-		self.gen_batch_size = 100
+		
 		# TOTAL SITUATIONS = different_boards x batch_size
-		# how many files to create (single element = ~22kB)
-		self.gen_num_files = 1
+		self.gen_different_boards = 50 # how many solved poker situations are generated
+		self.gen_batch_size = 100 # how many poker situations are solved simultaneously during data generation
+		self.gen_num_files = 1 # how many files to create (single element = ~22kB)
+		self.cfr_iters = 100 # the number of iterations that DeepStack runs CFR for
+		self.cfr_skip_iters = 60 # the number of preliminary CFR iterations which DeepStack doesn't factor into the average strategy (included in cfr_iters) 
+		self.batch_size = 256
+		self.num_epochs = 50
+		self.save_epoch = 2 # how often to save the model during training
+
+		# FOR TESTING
+		# self.gen_batch_size = 1
+		# self.cfr_iters = 1
+		# self.cfr_skip_iters = 0
+		# self.batch_size = 16
+		# self.num_epochs = 1
+		# self.save_epoch = 1
 
 		assert(self.gen_different_boards % self.gen_num_files == 0)
 
